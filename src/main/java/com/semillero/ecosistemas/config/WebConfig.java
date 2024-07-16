9cdf75b73061de47db02f088cda2ad3487eb4ada
Nico <nicolas2289h@gmail.com>
2024-07-15 21:19:01 -0300

    feat: add cors

--- src/main/java/com/semillero/ecosistemas/config/WebConfig.java
+++ src/main/java/com/semillero/ecosistemas/config/WebConfig.java
@@ -12,8 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:5173", "http://localhost:5173")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .exposedHeaders("abc")
